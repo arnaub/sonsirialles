5e4ee89c119b66579bf4c8bb9726099143b121a7
arnau <arnaubesorasans@MacBook-de-Arnau-Besora-Sans.local>
2012-10-03 11:36:42 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sonsirialles::Application.config.secret_token = '049ce10053bb7be56db7854c773078fb7819e38b3b71866e303cb99de5157440fc8b2e5de00281e8c19a6541aaac731375954fb4080ce396d9ce9fbae2e26850'
